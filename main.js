6658fa90d4c74a68eb5752f533e7f2e203b64412
Andrei <catrina.andrei93@yahoo.com>
2022-07-08 20:46:50 +0300

    FAQ section implemented

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 100);
 });
+
+
+// show/hide faq answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    //change icon
+
+    const icon = faq.querySelector('.faq__icon i');
+    if(icon.className ==="fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus"
+    } else {
+      icon.className = "fa-solid fa-plus"
+    }
+  })
+})
